ce9c77c33474132a26e7a8c1c88e5dfc08871952
Volkov Vladimir <vladimirwolkov@mail.ru>
2018-10-17 22:28:26 +0300

    Initial commit

---
+++ DEV-1/ArgsToString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace Dev1
+{
+
+    static class ArgsToString
+    {
+        /// <summary>
+        /// The ArgsToString class that forms a string from the command line arguments
+        /// </summary>
+        public static string ArrayToString(this string[] args)
+        {
+            StringBuilder concatenatedString = new StringBuilder();
+
+            foreach (string i in args)
+            {
+                concatenatedString.Append(i + " ");
+            }
+
+            concatenatedString.Remove(concatenatedString.Length - 1, 1); // Delete last space bar
+
+            return concatenatedString.ToString();
+        }
+    }
+}
---
+++ DEV-1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+
+
+namespace Dev1
+{
+	/// <summary>
+	/// MaximuNumberOfUnequalConsecutive
+	/// searches maximum number of unequal symbols in the string
+	/// and searches some exceptions
+	/// </summary>
+	class MaximuNumberOfUnequalConsecutive
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length != 0)// Checking the condition for the argument
+			{
+				MaximuNumberOfUnequalConsecutive EntryPointObject = new MaximuNumberOfUnequalConsecutive();
+				string inputString = args.ArrayToString(); // Form string from arguments array
+
+				Console.WriteLine(EntryPointObject.SearchLengthofUnequalSymbol(inputString));
+			}
+			else
+			{
+				Console.WriteLine("Exception message: wrong arguments number.");
+				return;
+			}
+		}
+
+		/// <summary> 
+		/// Method SearchLengthofUnequalSymbol
+		/// Function for finding the maximum number of consecutive unidentical elements in succession
+		/// </summary> 
+		/// <param name="stringArgument">String, which was inputed</param> 
+		/// <returns>Maximum number of unequal consecutive symbol in a string</returns>
+		int SearchLengthofUnequalSymbol(String stringArgument)
+		{
+			string buffer = ""; //Temporal storage for subsequences
+			int MaximumSubsequenceLength = 0;
+
+			for (int i = 0; i < stringArgument.Length; i++)
+			{
+				if (buffer.Contains(stringArgument[i]))
+				{
+					i -= (buffer.Length - buffer.LastIndexOf(stringArgument[i]));
+					buffer = "";
+				}
+				else
+				{
+					buffer = buffer + stringArgument[i];
+					if (MaximumSubsequenceLength < buffer.Length)
+					{
+						MaximumSubsequenceLength = buffer.Length;
+					}
+				}
+			}
+
+			return MaximumSubsequenceLength;
+		}
+	}
+}

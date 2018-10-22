58cc932f2bc85c76f4e3d96ccc8d6340c6997724
Volkov Vladimir <vladimirwolkov@mail.ru>
2018-10-22 23:44:31 +0300

    Task DEV-3 skeleton classes

---
+++ DEV-3/DEV-3/EntryPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DEV_3
+{
+    /// <summary> 
+    /// This class accept from the command line two arguments: 
+    /// 1) the number that needs to be convert to another numeral system
+    /// 2) the foundation of a new  numeral system 
+    /// </summary> 
+    class EntryPoint
+    {       
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ DEV-3/DEV-3/NumeralSystemConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+
+namespace DEV_3
+{
+    /// <summary> 
+    /// This class converts decimal number in other numeral system
+    /// </summary>
+    class NumeralSystemConverter
+    {
+    }
+}

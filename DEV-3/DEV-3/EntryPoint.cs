aab9f4e9c837bc9d09d342fa3cf57f50ef5addc6
Volkov Vladimir <vladimirwolkov@mail.ru>
2018-10-23 01:23:16 +0300

    working programm

--- DEV-3/DEV-3/EntryPoint.cs
+++ DEV-3/DEV-3/EntryPoint.cs
@@ -3,14 +3,44 @@
 namespace DEV_3
 {
     /// <summary> 
-    /// This class accept from the command line two arguments: 
-    /// 1) the number that needs to be convert to another numeral system
-    /// 2) the foundation of a new  numeral system 
+    /// EntryPoint accept from the command line two arguments
+    /// and convert number (1 argument) in the new numeral system (2 argument)
     /// </summary> 
     class EntryPoint
     {       
         static void Main(string[] args)
         {
+            try
+            {
+                int number = int.Parse(args[0]);
+                int newBase = int.Parse(args[1]);                
+                if (newBase < 2 || newBase > 20)
+                {
+                    throw new ArgumentOutOfRangeException("wrong base of new numeral system.");
+                }                
+                NumeralSystemConverter EntryPointObject = new NumeralSystemConverter();
+                if (number >= 0)
+                {
+                    Console.WriteLine(number + " in the " + newBase + " numeral system = " + EntryPointObject.DecimalToOtherNumeralSystem(number, newBase));
+                }
+                else
+                {
+                    Console.WriteLine(number + " in the " + newBase + " numeral system =  -" + EntryPointObject.DecimalToOtherNumeralSystem(number, newBase));
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Exception message: strings contain wrong symbols or null.");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Exception message: " + ex.ParamName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception message: " + ex.Message);
+            }            
         }
     }
 }
+
--- DEV-3/DEV-3/NumeralSystemConverter.cs
+++ DEV-3/DEV-3/NumeralSystemConverter.cs
@@ -8,5 +8,50 @@ namespace DEV_3
     /// </summary>
     class NumeralSystemConverter
     {
+        /// <summary> 
+        /// Method DecimalToOtherNumeralSystem
+        /// A method that creates a string that is a representation of a number in the new numeral system
+        /// </summary> 
+        /// <returns>Value In New System</returns>
+        public string DecimalToOtherNumeralSystem(int number, int newBase)
+        {            
+            StringBuilder valueInNewSystem = new StringBuilder();
+            int numberToAdd;
+            do
+            {
+                numberToAdd = number % newBase;
+                valueInNewSystem.Append(FigureConverter(numberToAdd));
+                number = number / newBase;
+
+            } while (0 != number);
+            string ConvertedString;
+            ConvertedString = valueInNewSystem.ToString();
+            return ReverseString(ConvertedString);
+        }
+
+        /// <summary>
+        /// Converts decimal numeral to new numeral  numeral system
+        /// </summary>
+        /// <param name="numbertForTranslation"></param>
+        /// <returns></returns>
+        private string FigureConverter(int numbertForTranslation)
+        {
+            char figureInNewSystem;
+            string possibleValues = "0123456789ABCDEFGHIJ";
+            figureInNewSystem = possibleValues[Math.Abs(numbertForTranslation)];
+            return Convert.ToString(figureInNewSystem);
+        }
+
+        /// <summary>
+        /// Reverses the order of the elements in a  string
+        /// </summary>
+        /// <param name="stringForReverse">string for reverse</param>
+        /// <returns></returns>
+        private string ReverseString(string stringForReverse)
+        {
+            char[] reversedString = stringForReverse.ToCharArray();
+            Array.Reverse(reversedString);
+            return new string(reversedString);
+        }        
     }
 }

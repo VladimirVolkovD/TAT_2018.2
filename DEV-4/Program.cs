03892e7ffbcfd52f03613f96ef054525dac915dd
Volkov Vladimir <vladimirwolkov@mail.ru>
2018-11-18 21:50:05 +0300

    My trouble with folder

---
+++ DEV-4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace DEV_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string xmlString = File.ReadAllText(@args[0]);
+                XmlParser.Pars(xmlString);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Error: " + error.Message);
+            }
+        }        
+    }
+}
+        
+
+    
+
+
